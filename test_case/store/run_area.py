8422a731ac8e9cf50677e125008ce48c6ee73e0d
shenfeicheng <1300521789@qq.com>
2024-05-23 17:08:09 +0800

    vs自动化测试

--- test_case/store/run_area.py
+++ test_case/store/run_area.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     test_files = [
         "test_area.py",
     ]
-    if os.path.exists("../Report/newxml"):
-        shutil.rmtree("../Report/newxml")
-    pytest_args = ["-vs"] + test_files + ["--alluredir", "../Report/newxml"]
+    if os.path.exists("../../Report/newxml"):
+        shutil.rmtree("../../Report/newxml")
+    pytest_args = ["-vs"] + test_files + ["--alluredir", "../../Report/newxml"]
     pytest.main(pytest_args)
